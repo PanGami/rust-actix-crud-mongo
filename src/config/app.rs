35b7218c3b6695380c447996217d58f23cdb10a0
PanGami <pancanugrahawiwcaksananansbw@gmail.com>
2022-10-19 12:32:37 +0700

    add signup endpoint

--- src/config/app.rs
+++ src/config/app.rs
@@ -16,7 +16,7 @@ pub fn config_path(config: &mut web::ServiceConfig) {
                 web::scope("api")
                 .route("", web::get().to(index)) //default api endpoint                      
                 .service(
-                    web::scope("user") // http://localhost:8080/api/tweets                            
+                    web::scope("user") // http://localhost:8080/api/user                            
                         .route("/signup", web::post().to(users::create))
                         //.route("/signup", web::post().to(tweets::create))
                         // .route("", web::get().to(tweets::index)) 
